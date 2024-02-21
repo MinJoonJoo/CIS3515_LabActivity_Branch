40bdd2dbfe83fffe6791cd8bbf54b7dca9d13e54
MinJoon Joo <tuk34896@temple.edu>
2024-02-21 15:22:52 -0500

    var to private, added some properties for edit text and button
    created new properties that were missing, and created nameEditText.text inside string.xml for the responds

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -9,18 +9,22 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
-    lateinit var displayTextView: TextView
-
+    private lateinit var displayTextView: TextView//make it private
+    private lateinit var nameEditText: EditText
+    private lateinit var clickMeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            val name = nameEditText.text
+            displayTextView.text = getString(R.string.greeting_text, name)
         }
 
 
