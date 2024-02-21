0fe7bac63d30f648e46b688cb8f5fbd89b64993e
MinJoon Joo <tuk34896@temple.edu>
2024-02-21 15:45:09 -0500

    changed button part with if statement
    created if, if the input is empty send an error message

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -8,25 +8,26 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    // Declare view properties - the first one is done for you
-    private lateinit var displayTextView: TextView//make it private
+    private lateinit var displayTextView: TextView
     private lateinit var nameEditText: EditText
     private lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
         nameEditText = findViewById(R.id.nameEditText)
         clickMeButton = findViewById(R.id.clickMeButton)
 
-        
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            val name = nameEditText.text
-            displayTextView.text = getString(R.string.greeting_text, name)
+            val name = nameEditText.text.toString().trim()
+
+            if (name.isEmpty()) {
+                displayTextView.text = getString(R.string.error_message_no_name)
+            } else {
+                displayTextView.text = getString(R.string.greeting_text, name)
+            }
         }
-
-
     }
 }
